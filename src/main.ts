a2f7ed6cf22f2a018cb8fb3a5d965e4e2a57570d
Agustin Bereciartua Castillo <galenzo17@users.noreply.github.com>
2024-09-02 22:28:10 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,9 @@
 import { NestFactory } from '@nestjs/core';
+import * as express from 'express';
 import { AppModule } from './app.module';
-import { Server } from 'http';
-import { Callback, Context, Handler } from 'aws-lambda';
-import serverlessExpress from '@vendia/serverless-express';
 
-let server: Handler;
-
-async function bootstrap(): Promise<Handler> {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();  // Enable CORS if needed
-  await app.init();
-
-  const expressApp = app.getHttpAdapter().getInstance();
-  return serverlessExpress({ app: expressApp });
+async function bootstrap() {
+  const app = await NestFactory.create<express.Express>(AppModule);
+  await app.listen(3000);
 }
-
-export const handler: Handler = async (event: any, context: Context, callback: Callback) => {
-  server = server ?? (await bootstrap());
-  return server(event, context, callback);
-};
+bootstrap();
