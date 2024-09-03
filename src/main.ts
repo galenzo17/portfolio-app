4d4feee8d4a55835fa3ec336ab7614b234926112
galenzo17 <bereciartua.agustin@gmail.com>
2024-09-02 22:45:27 -0400

    update main

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,12 @@
-import * as express from 'express';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module'; // Importa tu módulo principal de la aplicación
-import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<express.Application>(AppModule);
-  
-  // Configura el middleware para validar las solicitudes POST y GET automáticamente con 'ValidationPipe' por ejemplo:
-  app.use(new ValidationPipe());
+  const app = await NestFactory.create(AppModule);
 
-  if (process.env.NODE_ENV === 'production') {
-    const PORT = process.env.PORT || 3000;
-    await app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`)); // Configura el puerto si es necesario (por ejemplo, cambiando de '3000' a la variable del entorno PORT).
-  } else {
-    await app.start(); // Inicia tu aplicación en modo desarrollo para pruebas y depuración automáticas:
-  }
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
+  console.log(`Server is running on http://localhost:${PORT}`);
 }
+
 bootstrap();
